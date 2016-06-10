ec4479347f2f58d0dae3a3c5b453667fa0fd3788
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-06-10 16:59:11 -0400

    new landing too

--- app/controllers/equipment_categories_controller.rb
+++ app/controllers/equipment_categories_controller.rb
@@ -4,15 +4,7 @@ class EquipmentCategoriesController < ApplicationController
 
   def index
     @equipment_categories = EquipmentCategory.all
-     render :index
-      # if params[:search]
-      #   @rentals = Rental.search(params[:search]).
-      #     order(:name).
-      #     page(params[:page])
-      # else
-
-      # end
-    end
+  end
 
   def show
     @equipment_categories = EquipmentCategory.all
